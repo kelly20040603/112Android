fa9c3394daf5d78a9930ba8bcbc6ae769e531a92
kelly20040603 <kelly20040603@gmail.com>
2024-06-13 17:17:24 +0800

    2024.06.13

--- 2024.03.28/BMI2/app/src/main/java/com/example/bmi/MainActivity.java
+++ 2024.03.28/BMI2/app/src/main/java/com/example/bmi/MainActivity.java
@@ -17,18 +17,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txvShow = (TextView) findViewById(R.id.txvShow);
+        txvShow = findViewById(R.id.txvShow);
         txvShow.setTextSize(36);
-        EditText edtHeight = (EditText) findViewById(R.id.edtHeight);
-        EditText edtWeight = (EditText) findViewById(R.id.edtWeight);
-        Button btnCalc = (Button) findViewById(R.id.btnCalc);
-        Button btnClear = (Button) findViewById(R.id.btnClear);
+        EditText edtHeight = findViewById(R.id.edtHeight);
+        EditText edtWeight = findViewById(R.id.edtWeight);
+        Button btnCalc = findViewById(R.id.btnCalc);
+        Button btnClear = findViewById(R.id.btnClear);
+
         btnCalc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Check if EditText fields are empty
+                if (edtHeight.getText().toString().isEmpty() || edtWeight.getText().toString().isEmpty()) {
+                    // Display error message
+                    txvShow.setText("請輸入身高體重");
+                    txvShow.setTextColor(Color.RED);
+                    return; // Exit onClick method
+                }
+
+                // Check if height or weight is 0
                 double height = Double.parseDouble(edtHeight.getText().toString());
                 double weight = Double.parseDouble(edtWeight.getText().toString());
+                if (height == 0 || weight == 0) {
+                    // Display error message
+                    txvShow.setText("身高和體重不能為零");
+                    txvShow.setTextColor(Color.RED);
+                    return; // Exit onClick method
+                }
+
+                // Perform BMI calculation
                 double bmi = weight / Math.pow(height / 100.0, 2);
+
+                // Set text color based on BMI value
                 if (bmi >= 24)
                     txvShow.setTextColor(Color.RED);
                 else if (bmi < 18.5)
@@ -36,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
                 else
                     txvShow.setTextColor(Color.GREEN);
 
+                // Display calculated BMI
                 txvShow.setText(String.format("%.2f", bmi));
             }
         });
